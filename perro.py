8bf66f0a49750a7fab2b29f355ee780c33f2f720
Lorena Moldes <lorena.moldes@gmail.com>
2021-04-22 11:36:19 +0200

    primera versión de perro

---
+++ perro.py
@@ -0,0 +1,9 @@
+#El nombre va con la primera con mayusculas por convencion.
+
+class Perro():
+    #Siempre empieza con la función constructor
+    def __init__(self,nombre,edad,peso):
+        self-nombre= nombre
+        self.edad=edad
+        self.peso=peso
+    
